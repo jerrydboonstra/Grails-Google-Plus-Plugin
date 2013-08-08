416b086fac7ff7fb8fa1d3f1d2f6ecd9ad10340f
Jerry Boonstra <jerry@frequency.com>
2013-08-07 18:26:05 -0700

    add state and login_hint

--- src/groovy/org/grails/plugins/googlePlus/Person/GooglePlusUtil.groovy
+++ src/groovy/org/grails/plugins/googlePlus/Person/GooglePlusUtil.groovy
@@ -8,6 +8,21 @@ class GooglePlusUtil {
         String clientId = ConfigurationHolder.config.grails.plugins.googlePlus.clientId
         String callbackUrl = ConfigurationHolder.config.grails.plugins.googlePlus.callBackUrl
         String scope = ConfigurationHolder.config.grails.plugins.googlePlus.scope ?: 'https://www.googleapis.com/auth/plus.me'
-        return "https://accounts.google.com/o/oauth2/auth?scope=${scope}&redirect_uri=${callbackUrl}&response_type=code&client_id=${clientId}&access_type=offline"
+        String accessType = ConfigurationHolder.config.grails.plugins.googlePlus.accessType ?: 'offline'
+        def requiredParams = [:]
+        requiredParams.put 'scope', scope
+        requiredParams.put 'redirect_uri', callbackUrl
+        requiredParams.put 'response_type', 'code'
+        requiredParams.put 'client_id', clientId
+        requiredParams.put 'access_type', accessType
+
+        String state = ConfigurationHolder.config.grails.plugins.googlePlus.state ?: ''
+        String loginHint = ConfigurationHolder.config.grails.plugins.googlePlus.loginHint ?: ''
+        def optionalParams = [:]
+        if (state) optionalParams.put 'state', state
+        if (loginHint) optionalParams.put 'login_hint', loginHint
+
+        def queryParams = (requiredParams + optionalParams).collect { k,v -> "${k}=${v}" }.join('&')
+        return "https://accounts.google.com/o/oauth2/auth?${queryParams}"
     }
 }
